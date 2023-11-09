7a13ee342e5d59121b2c2188b0d4af95036f6895
vasi334 <74433291+vasi334@users.noreply.github.com>
2023-11-09 23:15:28 +0200

    initializare proiect

---
+++ backend_project/src/test/java/com/gymbuddy/backend_project/BackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymbuddy.backend_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e741cf4adaa8a7a15ba7dc5a17d010b589182a1a
Diana <diana.furca@stud.ubbcluj.ro>
2023-11-23 14:27:14 +0200

    Implementare DTOs pentru modelele din baza de date

--- backend_project/src/main/java/com/gymbuddy/backend_project/BackendProjectApplication.java
+++ backend_project/src/main/java/com/gymbuddy/backend_project/BackendProjectApplication.java
@@ -2,9 +2,12 @@ package com.gymbuddy.backend_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-public class BackendProjectApplication {
+@EntityScan(basePackages = "com.gymbuddy.backend_project.entity")
+public class BackendProjectApplication
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendProjectApplication.class, args);

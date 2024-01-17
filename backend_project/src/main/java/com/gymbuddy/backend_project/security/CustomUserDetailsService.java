e8156c0f525d102d832646725673c6beb9931c47
vasi334 <vasihoroi@yahoo.com>
2024-01-18 01:33:15 +0200

    added necessary comments

--- backend_project/src/main/java/com/gymbuddy/backend_project/security/CustomUserDetailsService.java
+++ backend_project/src/main/java/com/gymbuddy/backend_project/security/CustomUserDetailsService.java
@@ -18,25 +18,33 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private UserRepository userRepository;
 
+    // Constructor to inject UserRepository
     public CustomUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    // Implementation of the UserDetailsService interface method
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // Retrieve user from the repository by email
         User user = userRepository.findByEmail(email);
 
+        // Check if the user exists
         if (user != null) {
+            // Return a UserDetails object with username, password, and authorities (roles)
             return new org.springframework.security.core.userdetails.User(user.getEmail(),
                     user.getPassword(),
                     mapRolesToAuthorities(user.getRoles()));
-        }else{
+        } else {
+            // Throw an exception if the user is not found
             throw new UsernameNotFoundException("Invalid username or password.");
         }
     }
 
-    private Collection < ? extends GrantedAuthority> mapRolesToAuthorities(Collection <Role> roles) {
-        Collection < ? extends GrantedAuthority> mapRoles = roles.stream()
+    // Helper method to map roles to authorities
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        // Map roles to SimpleGrantedAuthority and collect into a list
+        Collection<? extends GrantedAuthority> mapRoles = roles.stream()
                 .map(role -> new SimpleGrantedAuthority(role.getName()))
                 .collect(Collectors.toList());
         return mapRoles;
